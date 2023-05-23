2f743394a0c20ec2871e0e270e1ae473444f1a21
Denis Chetvertkov <chetvertkov-denis96@mail.ru>
2023-05-23 21:42:51 +0300

    Changed calc

---
+++ src/Calculator.java
@@ -0,0 +1,28 @@
+import java.util.function.*;
+
+public class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> devide = (x, y) ->   {
+        int result;
+        if (y == 0) {
+            System.out.println("Ошибка: деление на ноль!");
+            result = Integer.MIN_VALUE;
+        } else {
+            result = x / y;
+        }
+        return result;
+    };
+
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+
+    Predicate<Integer> isPositive = x -> x > 0;
+
+    Consumer<Integer> println = System.out::println;
+
+    private Calculator() {}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(10, 2);
+        int b = calc.minus.apply(3,1);
+        int c = calc.devide.apply(a,b);
+        calc.println.accept(c);
     }
 }
